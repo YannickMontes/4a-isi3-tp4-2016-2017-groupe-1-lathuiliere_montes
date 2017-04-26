24455cb2ce8e35b1e6cd44480861ae8015f02697
YoannLTH <yoann.lathuiliere@gmail.com>
2017-04-26 10:41:56 +0200

    [WIP] : MVC pattern

--- src/main/java/Model/Segment.java
+++ src/main/java/Model/Segment.java
@@ -16,13 +16,4 @@ public class Segment
         ptStart = new Point(0,0);
         ptEnd = new Point(0,0);
     }
-
-    public void drawSegment(Graphics graph)
-    {
-        if (graph==null)
-            return;
-
-        graph.setColor(color);
-        graph.drawLine(ptStart.x, ptStart.y, ptEnd.x, ptEnd.y);
-    }
 }
--- src/main/java/View/SegmentView.java
+++ src/main/java/View/SegmentView.java
@@ -1,8 +1,26 @@
 package View;
 
+import Model.Segment;
+
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Created by yannick on 26/04/17.
  */
-public class SegmentView
+public class SegmentView extends JComponent
 {
+    private Segment segment;
+
+    public SegmentView(Segment segment) {
+        this.segment = segment;
+    }
+
+    public void drawSegment(Graphics graph)
+    {
+        if (graph != null) {
+            graph.setColor(segment.color);
+            graph.drawLine(segment.ptStart.x, segment.ptStart.y, segment.ptEnd.x, segment.ptEnd.y);
+        }
+    }
 }
